ead471942b389f91a9aa29e9ae604a518742d157
Hleb Aksionau <glebaks94@gmail.com>
2018-01-26 16:26:21 +0300

    init template

---
+++ src/main/java/by/tc/task01/entity/criteria/Criteria.java
@@ -0,0 +1,27 @@
+package by.tc.task01.entity.criteria;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Criteria<E> {
+	
+	private String applianceType;
+
+	private Map<E, Object> criteria = new HashMap<E, Object>();
+
+	public void add(E searchCriteria, Object value) {
+		criteria.put(searchCriteria, value);
+	}
+
+	public String getApplianceType() {
+		return applianceType;
+	}
+
+	public void setApplianceType(String applianceType) {
+		this.applianceType = applianceType;
+	}
+		
+	
+	// you may add your own code here
+
+}
