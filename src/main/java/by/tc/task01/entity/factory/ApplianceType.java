c1a3cf6814599c699a1f5446423e3ee4848ee828
OSmol <Olga_Smolyakova@outlook.com>
2018-02-21 12:29:37 +0300

    Update ApplianceType.java

--- src/main/java/by/tc/task01/entity/factory/ApplianceType.java
+++ src/main/java/by/tc/task01/entity/factory/ApplianceType.java
@@ -4,7 +4,8 @@ public enum ApplianceType {
 	
 	OVEN, LAPTOP, REFREGERATOR, VACUUME_CLEANER, TABLET_PC, SPEAKERS;
 	
-	public static ApplianceType getApplianceType(String name){
+	public static ApplianceType getApplianceType(String name){// использование стандартного valueOf для возможности расширения этого перечисления выглядит 
+		// более уместным, по крайне мере в метод не надо будет вмешиваться
 		switch (name){
 		case "VacuumeCleaner": return VACUUME_CLEANER;
 		case "TABLET_PC": return TABLET_PC;
