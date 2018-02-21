9ee1973b3910375671506dc13e87b6b2c1c463f8
OSmol <Olga_Smolyakova@outlook.com>
2018-02-21 12:04:14 +0300

    Update ApplianceServiceImpl.java

--- src/main/java/by/tc/task01/service/impl/ApplianceServiceImpl.java
+++ src/main/java/by/tc/task01/service/impl/ApplianceServiceImpl.java
@@ -15,7 +15,7 @@ public class ApplianceServiceImpl implements ApplianceService{
 
 	public <E> List<Appliance> find(Criteria<E> criteria) {
 		if (!Validator.criteriaValidator(criteria)) {
-			return null;
+			return null; // к моменту написания тобой этого кода мы уже разобрались с исключччениями
 		}
 		List<Appliance> appliances=new ArrayList<Appliance>();
 		DAOFactory factory = DAOFactory.getInstance();
@@ -24,7 +24,8 @@ public class ApplianceServiceImpl implements ApplianceService{
 			appliances = applianceDAO.find(criteria);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
+			e.printStackTrace();// счто это за обработка? Ты зачем гасишь исключение, не говоря вызывающему коду, что у тебя что-то слудилось
+			// возврат пустого списка означает, что таких товар не найдено - штатная ситуация, а не что при выполнении кода что-то пошло не так...
 		}
 		
 		return appliances;
